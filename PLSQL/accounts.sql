a33a4468d3d4c5ab045a8e3fb20f87f1c3619f6b
rolansy <ronalshoeygeorge@gmail.com>
2024-08-24 11:51:31 +0530

    feat: Add PL/SQL script to create accounts table

---
+++ PLSQL/accounts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE accounts (
+    acc_num NUMBER(5) NOT NULL PRIMARY KEY,
+    bal NUMBER(10)
+);
+
+INSERT INTO accounts values(1245,10000);
+INSERT INTO accounts values(6980,50000);
+INSERT INTO accounts values(7245,2000);
