d68798afcb9c45a733cef1d41b3a6a34afbe57a0
rolansy <ronalshoeygeorge@gmail.com>
2024-08-27 09:37:39 +0530

    feat: Add PL/SQL script to calculate average marks and display current date and day

---
+++ PLSQL/marks.sql
@@ -0,0 +1,21 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    avg_marks NUMBER(10);
+    cdate DATE;
+    cweek varchar2(10); 
+
+BEGIN
+    SELECT AVG(MARKS) INTO avg_marks FROM STUDENTS;
+    DBMS_OUTPUT.PUT_LINE('Average Marks : '||avg_marks);
+    if avg_marks<40 then
+        dbms_output.put_line('Warning! Low Average Marks Need Improvement! ');
+    else
+        dbms_output.put_line('Good Average Marks! ');
+    END if;
+    cdate := SYSDATE;
+    DBMS_OUTPUT.PUT_LINE('Current Date : '||cdate);
+    cweek := TO_CHAR(cdate,'DAY');
+    DBMS_OUTPUT.PUT_LINE('Current Day : '||cweek);
+END;
+/
