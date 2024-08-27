31fe748d726e1f977f630086d3b1fe1ca7e5be08
rolansy <ronalshoeygeorge@gmail.com>
2024-08-27 11:07:25 +0530

    feat: Add category column to customer_balance table in PL/SQL script

--- PLSQL/DBS/customer_balance.sql
+++ PLSQL/DBS/customer_balance.sql
@@ -10,3 +10,5 @@ INSERT INTO customer_balance (CID, bal) VALUES (8756, 69000);
 INSERT INTO customer_balance (CID, bal) VALUES (4200, 500);
 INSERT INTO customer_balance (CID, bal) VALUES (6902, 12003);
 INSERT INTO customer_balance (CID, bal) VALUES (2030, 78030);
+
+ALTER TABLE customer_balance ADD Category VARCHAR(20);
