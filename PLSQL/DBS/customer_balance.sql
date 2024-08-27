eb9908e28390407d22f485dce1e620c06049f370
rolansy <ronalshoeygeorge@gmail.com>
2024-08-27 10:18:39 +0530

    feat: Add PL/SQL script to create customer_balance table and insert initial data

---
+++ PLSQL/DBS/customer_balance.sql
@@ -0,0 +1,12 @@
+CREATE TABLE customer_balance (
+    CID NUMBER(5) NOT NULL PRIMARY KEY,
+    bal NUMBER(10)
+);
+
+INSERT INTO customer_balance (CID, bal) VALUES (6901, 10340);
+INSERT INTO customer_balance (CID, bal) VALUES (6950, 7830);
+INSERT INTO customer_balance (CID, bal) VALUES (7001, 45000);
+INSERT INTO customer_balance (CID, bal) VALUES (8756, 69000);
+INSERT INTO customer_balance (CID, bal) VALUES (4200, 500);
+INSERT INTO customer_balance (CID, bal) VALUES (6902, 12003);
+INSERT INTO customer_balance (CID, bal) VALUES (2030, 78030);
