b3a9a0435d488f3c2f1811da8de1621839d733b5
rolansy <ronalshoeygeorge@gmail.com>
2024-08-27 09:16:25 +0530

    min balance detect

--- PLSQL/bank.sql
+++ PLSQL/bank.sql
@@ -15,6 +15,9 @@ BEGIN
     UPDATE accounts SET bal=bal+credit WHERE acc_num=&acc;
     SELECT bal INTO balance FROM accounts where acc_num=&acc;
     DBMS_OUTPUT.PUT_LINE('Balance : '||balance);
+    if balance<500 then
+        dbms_output.put_line('Warning! Maintain Minimum Balance! ');
+    END if;
 END;
 /
 
