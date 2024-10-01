8bce5f64845c73efeaf0a8ef4b39ee93bec8a9dd
rolansy <ronalshoeygeorge@gmail.com>
2024-10-01 11:35:47 +0530

    feat: Add new train records to the PL/SQL script

--- PLSQL/DBS/train.sql
+++ PLSQL/DBS/train.sql
@@ -21,3 +21,40 @@ ROLLBACK;
 
 
 SELECT * FROM Train;
+
+
+
+SQL> INSERT INTO Train (TName, TDate, Destination, Seats) VALUES ('Express', TO_DATE('2023-10-01', 'YYYY-MM-DD'), 'Delhi', 100);
+
+1 row created.
+
+SQL> COMMIT;
+
+Commit complete.
+
+SQL> INSERT INTO Train (TName, TDate, Destination, Seats) VALUES ('Nirvana', TO_DATE('2023-10-02', 'YYYY-MM-DD'), 'Doha', 150);
+
+1 row created.
+
+SQL> SELECT * FROM Train;
+
+   TRAINNO TNAME           TDATE              DESTINATION          SEATS
+---------- --------------- ------------------ --------------- ----------
+         4 Express         01-OCT-23          Delhi                  100
+         5 Nirvana         02-OCT-23          Doha                   150
+
+SQL> INSERT INTO Train (TName, TDate, Destination, Seats) VALUES ('Vazha', TO_DATE('2023-10-02', 'YYYY-MM-DD'), 'Shimla', 150);
+
+1 row created.
+
+SQL> SELECT * FROM Train;
+
+   TRAINNO TNAME           TDATE              DESTINATION          SEATS
+---------- --------------- ------------------ --------------- ----------
+         4 Express         01-OCT-23          Delhi                  100
+         5 Nirvana         02-OCT-23          Doha                   150
+         6 Vazha           02-OCT-23          Shimla                 150
+
+SQL> ROLLBACK;
+
+Rollback complete.
