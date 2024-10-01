586a640e0a006765f93d92a930a4c111f4d8df7a
rolansy <ronalshoeygeorge@gmail.com>
2024-10-01 10:54:09 +0530

    feat: Add PL/SQL script to make and cancel reservations

--- PLSQL/DBS/railway.sql
+++ PLSQL/DBS/railway.sql
@@ -18,7 +18,29 @@ CREATE SEQUENCE TrainNo_seq
 START WITH 1
 INCREMENT BY 1;
 
--- Create sequence for ResID
+
 CREATE SEQUENCE ResID_seq
 START WITH 1
 INCREMENT BY 1;
+
+Select * from Railway;
+Select * from Reservation;
+
+INSERT INTO Railway (TName, TDate, Destination, Seats) VALUES ('Nelly', TO_DATE('2023-10-01', 'YYYY-MM-DD'), 'Cuba', 100);
+INSERT INTO Railway (TName, TDate, Destination, Seats) VALUES ('Nirvana', TO_DATE('2023-10-02', 'YYYY-MM-DD'), 'Doha', 150);
+
+
+
+
+
+SET SERVEROUTPUT ON;
+
+BEGIN
+    MakeReservation(p_TrainNo => 27, p_PassengerName => 'John Doe', p_SeatNo => 1);
+END;
+/
+
+BEGIN
+    CancelReservation(p_ResID => 1);
+END;
+/
